16aad29686a8262f1f55813a4a6dc930593d11f5
Avery Miller <avery@uconn.edu>
2019-11-04 19:01:41 -0500

    included filesearch + updated test data
    + filesearch.py filters through specific files&folders, especially in case of duplicates
    + Also added option to log issues
    + saves participant info as part of filename for 'complete_gaze_df.csv'
    
    + reformatted 'Test Data' folder to be more like the actual directory
    + small reformatting to PEP8 standards

---
+++ filesearch.py
@@ -0,0 +1,69 @@
+import os
+
+
+def findparticipants(dataroot):
+    included_participants = []
+
+    days = [day for day in os.listdir(dataroot)
+            if os.path.isdir(os.path.join(dataroot, day))]
+
+    for day in days:
+        dayfolder = os.path.join(dataroot, day)
+        participants = [participant for participant in os.listdir(dayfolder)
+                        if os.path.isdir(os.path.join(dayfolder, participant))]
+        for p in participants:
+            participant_root = os.path.join(dayfolder, p)
+            included_participants.append(participant_root)
+
+    return included_participants
+
+
+# uses the export folder with highest number
+def findhighest(path):
+    highest = ''
+    for exports in os.listdir(path):
+
+        if not highest:
+            highest = exports
+        else:
+            exp = int(exports)
+            high = int(highest)
+            if exp > high:
+                highest = exports
+
+    return os.path.join(path, highest)
+
+
+# search for .log file, but returns list if any duplicates found
+def findlogfile(root):
+    logfile = ''
+    duplicates = []
+
+    for file in os.listdir(root):
+        if file.endswith('.log'):
+            if not logfile:
+                logfile = os.path.join(root, file)
+            else:
+                dupilcate.append(file)
+
+    # if no duplicates found
+    if not duplicates:
+        return logfile
+
+    participant = os.path.split(root)[1]
+    day = os.path.split(os.path.split(root)[0])[1]
+
+    print('\nduplicates found for ' + str(day) + '_' + str(participant))
+    print(duplicates)
+
+    return duplicates
+
+
+# accounts for future ver where it uses 'info.xslx' + 'info.old_style.csv'
+def findinfofile(root):
+    # in future will include 'info.xlsx'
+    possiblenames = ('info.csv', 'info.old_style.csv')
+
+    for file in os.listdir(root):
+        if file in possiblenames:
+            return os.path.join(root, file)
--- run-sorting.py
+++ run-sorting.py
@@ -1,7 +1,5 @@
-
 # import libraries that we need
 import csv
-import gc
 import os
 import glob
 from datetime import datetime
@@ -9,57 +7,53 @@ import pandas as pd
 
 # import custom-made things that we'll need
 from sorting import Sorting, chunks, process_surfaces, pair_logs, associate_gaze_stimulus
-
-#-------- FILE PATHS SETUP --------#
-
-# NOTE!! This script should be ideally be placed at the top level of a participant's directory
-# That is what determines the "root" filepath!
-
-# After my 2 exams i'll make sure to implement a thing that is more interactive
-# or potentially a batch mode? if that's desired.
-#-=================================#
+from filesearch import findparticipants, findhighest, findlogfile, findinfofile
 
 # set root to current path location
-top_root = os.path.join(os.getcwd(), 'test data')
-
-# figure out the participants in the directory
-included_participants = [participant for participant in os.listdir(top_root)
-                        if os.path.isdir(os.path.join(top_root,participant))]
+top_root = os.path.join(os.getcwd(), 'data')
 
 # set or create directory for saving logs
 savelogs = os.path.join(os.getcwd(), 'savelogs')
 if not os.path.exists(savelogs):
     os.makedirs(savelogs)
 
+# keeps track of any issues and saves to file at end
+issues = {}
+
+# figure out the participants in each sub-directory
+# each participant = full path to their datafolder
+included_participants = findparticipants(dataroot)
+
 # cycle through participants
 for next_participant in included_participants:
 
     # set participant's working directory
-    root = os.path.join(top_root,next_participant)
+    root = next_participant
 
-    # TODO: make testdata directory selection more flexible
-
-    # get testdata
-    testdata = os.path.join(root,'000').replace('\\', '/')
-
-    # look for the exports folder
-    # TODO: This has to be made more robust.
-    exportfolder = os.path.join(testdata, 'exports', '004').replace('\\', '/')
-
-    # create paths for required participant output files
-    logfile = glob.glob(root+'/*.log')[0]
-
-    # TODO: implement a check to make sure there's no more than 1 logfile
-
-    # identify the export info file path
-    exportinfo = os.path.join(exportfolder, 'export_info.csv').replace('\\', '/')
+    # sets participant info for documentation purposes
+    participant = os.path.split(root)[1]
+    day = os.path.split(os.path.split(root)[0])[1]
+    participant_info = str(participant) + '_' + str(day)
 
     # identify the info file path
-    infofile = os.path.join(testdata, 'info.old_style.csv').replace('\\', '/')
+    infofile = findinfofile(root)
+
+    # create paths for required participant output files
+    logfile = findlogfile(root)
+    if type(logfile) == list:
+        # if multiple log files found, documents as issue and skip this participant
+        issues[participant_info] = logfile
+        continue
+
+    # look for the exports folder
+    exportfolder = findhighest(os.path.join(root, 'exports'))
+
+    # identify the export info file path
+    exportinfo = os.path.join(exportfolder, 'export_info.csv')
 
     # gaze/position file paths
-    gazesurface_file = glob.glob(exportfolder+'/surfaces/gaze_positions*.csv')[0]
-    surfaceevents = os.path.join(exportfolder, 'surfaces', 'surface_events.csv').replace('\\', '/')
+    gazesurface_file = glob.glob(exportfolder + '/surfaces/gaze_positions*.csv')[0]
+    surfaceevents = os.path.join(exportfolder, 'surfaces', 'surface_events.csv')
 
     # sort the info file
     sort = Sorting(infofile, savelogs)
@@ -74,10 +68,24 @@ for next_participant in included_participants:
     paired_logs = pair_logs(processed_surfaces, processed_logs)
 
     # associate the gaze data with the stimulus data
-    gaze_stimulus_df = associate_gaze_stimulus(gazesurface_file,paired_logs)
-
-    # TODO: update file savename to incorporate participant's ID
+    gaze_stimulus_df = associate_gaze_stimulus(gazesurface_file, paired_logs)
 
     # save the final dataframe
-    gaze_stimulus_df.to_csv(savelogs+'/complete_gaze_df.csv',
-                           index=None)
+    filename = participant_info + '_complete_gaze_df.csv'
+    gaze_stimulus_df.to_csv(savelogs + '/' + filename, index=None)
+
+
+# logs issues, if there are any
+# right now just for ".log" duplicates, more can be added though
+if issues:
+    date = str(datetime.now())
+
+    issuesfile = savelogs + '/issues.csv'
+    mode = "w+"
+    if os.path.exists(issuesfile):
+        mode = "a"
+
+    with open(issuesfile, mode, newline='') as f:
+        writer = csv.writer(f)
+        for participant, issue in issues.items():
+            writer.writerow([participant, issue])
