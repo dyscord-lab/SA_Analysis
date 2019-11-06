598291d9370c83c7978d43624615d115304bdd15
Alexandra Paxton <paxton.alexandra@gmail.com>
2019-11-05 20:49:41 -0500

    More logfile debugging

--- run-sorting.py
+++ run-sorting.py
@@ -37,7 +37,8 @@ for next_participant in included_participants:
 
     # identify the info file path
     print(root + '/../*.log')
-    infofile = glob.glob(os.path.join(root ,"../") + '*.log')[0]
+    infofile = (glob.glob(os.path.abspath(os.path.join(root ,"../"))
+                + '*.log')[0])
     print(infofile)
 
     # # create paths for required participant output files
